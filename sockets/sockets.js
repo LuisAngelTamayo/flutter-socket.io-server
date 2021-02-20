2d0bba3df3b0874fc05ac965cf677273741d0431
Luis Angel Tamayo <latzuniga@gmail.com>
2021-02-19 18:57:01 -0600

    Cambios crud desde flutter

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,14 +1,44 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+bands.addBand( new Band('Queen'));
+bands.addBand( new Band('Bon Jovi'));
+bands.addBand( new Band('Heroes del silencio'));
+bands.addBand( new Band('Metalica'));
+console.log(bands)
 //mensajes de Sockets ->
 io.on('connection', client => {
     console.log('cliente conectado')
-
+    client.emit('active-bands', bands.getBands());//para emitir a los clientes las bandas registradas
     client.on('disconnect', () => { console.log('cliente desconectado') });
 
-    client.on('Mensaje', ( payload ) => {
+    client.on('mensaje', ( payload ) => {
         console.log('Mensaje', payload );
 
 
-        io.emit('Mensaje', {admin: 'nuevo mensaje'});
+        io.emit('mensaje', {admin: 'nuevo mensaje'});
     });
+
+
+
+
+    client.on('vote-band', ( payload ) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());//para emitir a los clientes las bandas registradas
+    });
+
+
+    client.on('add-band', ( payload ) => {
+        const newBand = new Band(payload.name);
+        bands.addBand( newBand);
+        io.emit('active-bands', bands.getBands());//para emitir a los clientes las bandas registradas
+    });
+
+    client.on('delete-band', ( payload ) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());//para emitir a los clientes las bandas registradas
+    });
+  
 });
